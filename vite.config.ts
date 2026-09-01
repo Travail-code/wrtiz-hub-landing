a5058c4156d8acaaaabb14d5e9119ee617848c9f
Travail-code <yohantravail05@gmail.com>
2026-09-01 15:42:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
     tanstackStart(),
     viteReact(),
   ],
