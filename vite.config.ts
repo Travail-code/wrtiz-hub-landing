f558bffedbbf4b1ce4cdfba04fed84b2bb5f62c8
Travail-code <yohantravail05@gmail.com>
2026-09-01 15:02:16 +0200

    Simplify vite.config.ts: use only react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePlugin as router } from '@tanstack/router-plugin'
 
 export default defineConfig({
-  plugins: [
-    router(),
-    react(),
-  ],
+  plugins: [react()],
 })
