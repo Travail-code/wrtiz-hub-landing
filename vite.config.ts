536ea27eb884b3d033a0bb1e7787c6f3945b1280
Travail-code <yohantravail05@gmail.com>
2026-09-01 15:01:31 +0200

    Fix vite.config.ts: try router() export name

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import router from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
-    tanstackRouter(),
+    router(),
     react(),
   ],
 })
