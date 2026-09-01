6dc8374ae8d929402d52370d5fe277d5f97e5baa
Travail-code <yohantravail05@gmail.com>
2026-09-01 15:07:56 +0200

    Revert to minimal Vite config - TanStack Start works without special plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import start from '@tanstack/start/vite'
 
 export default defineConfig({
-  plugins: [
-    start(),
-    react(),
-  ],
+  plugins: [react()],
 })
