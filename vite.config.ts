e13d109ff83af89f73885b7bde24d52e42eb9322
Travail-code <yohantravail05@gmail.com>
2026-09-01 15:33:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import start from '@tanstack/start/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    start(),
-    react(),
+    tanstackStart(),
+    viteReact(),
   ],
 })
