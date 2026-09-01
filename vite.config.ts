648eb7467d2f1f60e23d6a62caba6cae150728df
Travail-code <yohantravail05@gmail.com>
2026-09-01 15:02:01 +0200

    Fix vite.config.ts: use vitePlugin as router export

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import router from '@tanstack/router-plugin/vite'
+import { vitePlugin as router } from '@tanstack/router-plugin'
 
 export default defineConfig({
   plugins: [
